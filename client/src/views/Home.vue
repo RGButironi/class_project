0383ca8820fc929c18bbda159de11f9173ebc5c5
Ronnie <ronaldbutironi@gmail.com>
2020-02-27 16:50:14 -0500

    Player card w/ score fature and current card done

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
   <section class="container">
-  <div class="hero">
-      <h1 class="title">
-        Home Page
-      </h1>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <ul>
-        <li v-for="player in Players" :key="player.Name">
-         {{player.Name}} {{player.Score}}
-        </li>
-      </ul>
+    <div class="hero">
+        <h1 class="title">
+          Home Page
+        </h1>
     </div>
+    <div class="card">
+      <div class="card-content">
+        <ul>
+          <li v-for="player in Players" :key="player.Name">
+            {{player.Name}} {{player.Score}}
+          </li>
+        </ul>
+      </div>
+
+      <img :src="CurrentPicture"  class="card-image"/>
     </div>
-    <img :src="CurrentPicture" />
-</section>
+  </section>
 </template>
 
+
+
+
 <script>
-import {Players, PictureDeck, CurrentPicture, Init} from "../models/Game";
+import { Players, PictureDeck, CurrentPicture, Init } from "../models/Game";
 Init();
 export default {
   name: 'Home',
@@ -33,5 +37,11 @@ export default {
   }
 }
 </script>
+
+<style >
+  img.card-image {
+    width: 100%
+  }
+</style>
 
 
